51ff9a94bf2c9a3ff819e72d606a225063971cb1
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-07-25 16:34:02 -0300

    Meu primeiro Commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
+
+Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/sobrenos', [SobreNosController::class, 'sobrenos']);
