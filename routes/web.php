96e992ae2b4574d4880637eeeb6ed9a2d5cd4435
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-07-30 17:53:42 -0300

    intruducao ao blade

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\TesteController;
+use App\Http\Controllers\FornecedoresController;
+
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
@@ -14,7 +16,7 @@ Route::get ('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get ('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get ('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', [FornecedoresController::class, 'index'])->name('app.fornecedores');
     Route::get ('/produtos', function(){return 'Produtos';})->name('app.produtos');
 
 });
