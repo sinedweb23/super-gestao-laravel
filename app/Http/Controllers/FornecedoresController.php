e1e635c6abbecb60762f7e382b90e2b0dd067f7f
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-07-31 17:19:54 -0300

    atualizacao do layout das views

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -7,7 +7,35 @@ use Illuminate\Http\Request;
 class FornecedoresController extends Controller
 {
     public function index(){
-        return view('site.fornecedor.index');
+        $fornecedores = [
+            0 =>[
+                'nome'=>'Fornecedor 1', 
+                'status'=>'N',
+                'cnpj' => '',
+                'ddd' => '11', // são paulo
+                'telefone'=> '5845454'
+            ],
+
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', // fortaleza
+                'telefone'=> '95665456'
+            ],
+
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32',//juiz de fora
+                'telefone'=> '795665456'
+            ]
+        ];
+
+      
+
+        return view('site.fornecedor.index', compact('fornecedores'));
         
 
     }
--- resources/views/site/fornecedor/index.blade.php
+++ resources/views/site/fornecedor/index.blade.php
@@ -1,1 +1,47 @@
-<h3>fornecedor</h3>
+<h3>Fornecedor</h3>
+
+{{-- Comentario no interpretador blade --}}
+
+@php 
+
+/*if(isset($variavel)) {}// retornar true se a variavel estiver definida
+
+*/
+@endphp
+
+{{--executa enquanto retorno for falso--}}
+
+@isset($fornecedores)
+
+@forelse ($fornecedores as $indice => $fornecedor)
+  Iteração atual: {{$loop->iteration}}
+<br>
+Fornecedor: {{$fornecedor['nome']}}
+<br>
+Status: {{$fornecedor['status']}}
+<br>
+CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}
+<br>
+Telefone: ({{$fornecedor['ddd'] ?? ''}}) {{$fornecedor ['telefone'] ?? ''}}
+<br>
+@if($loop->first)
+ Primeira iteração do loop
+@endif
+
+@if($loop->last)
+ Primeira iteração do loop
+
+ <br>
+ Total de registros: {{ $loop->count}}
+@endif
+<hr>
+
+
+@empty
+ NÃO EXISTEM FORNECEDORES CADASTRADOS
+@endforelse
+@endisset
+
+
+
+
